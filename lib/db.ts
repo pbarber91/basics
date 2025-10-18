06b252a7f566f0e1a18cc9427f31a59da590add1
pbarber91 <pjsmac@gmail.com>
2025-10-17 20:08:44 -0400

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,8 @@
+// lib/db.ts
 import { PrismaClient } from "@prisma/client";
-const g = globalThis as any;
-export const prisma = g.prisma ?? new PrismaClient();
-if (process.env.NODE_ENV !== "production") g.prisma = prisma;
+
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
